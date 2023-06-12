6414f8d5fa003fae027db5b17dfe15d109afc26d
Md Yaseen <mdyaseeny589@gmail.com>
2023-06-13 03:10:56 +0530

    User Controller implemented

--- src/main/java/com/example/vaccineManagementSystem/Controllers/UserController.java
+++ src/main/java/com/example/vaccineManagementSystem/Controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.vaccineManagementSystem.Controllers;
 
+import com.example.vaccineManagementSystem.Dtos.RequestDtos.UpdateEmailDto;
 import com.example.vaccineManagementSystem.Models.User;
 import com.example.vaccineManagementSystem.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,23 @@ public class UserController {
 
     @PostMapping("/addUser")
     public User addUser(@RequestBody User user){
+
         return userService.addUser(user);
     }
 
     @GetMapping("/getVaccinationDate")
     public Date getVaccinationDate(@RequestParam("userId") Integer userId){
         return userService.getVaccinationDate(userId);
+    }
+
+    @PutMapping("/updateEmail")
+    public String updateEmail(@RequestBody UpdateEmailDto updateEmailDto){
+        return userService.updateEmail(updateEmailDto);
+    }
+
+    @GetMapping("/getEmail/{emailId}")
+    public User getUserByEmail(@PathVariable("emailId") String emailId){
+        return userService.getUserByEmail(emailId);
     }
 
 }
--- src/main/java/com/example/vaccineManagementSystem/Services/UserService.java
+++ src/main/java/com/example/vaccineManagementSystem/Services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.vaccineManagementSystem.Services;
 
+import com.example.vaccineManagementSystem.Dtos.RequestDtos.UpdateEmailDto;
 import com.example.vaccineManagementSystem.Models.Dose;
 import com.example.vaccineManagementSystem.Models.User;
 import com.example.vaccineManagementSystem.Repository.UserRepository;
@@ -21,5 +22,19 @@ public class UserService {
         User user = userRepository.findById(userId).get();
         Dose dose = user.getDose();
         return dose.getVaccinationDate();
+    }
+
+    public String updateEmail(UpdateEmailDto updateEmailDto) {
+        User user = userRepository.findById(updateEmailDto.getUserId()).get();
+        user.setEmailId(updateEmailDto.getNewEmailId());
+
+        userRepository.save(user);
+
+        return "Old Email id is updated with " + updateEmailDto.getNewEmailId();
+    }
+
+    public User getUserByEmail(String emailId) {
+        User user = userRepository.findByEmailId(emailId);
+        return user;
     }
 }
